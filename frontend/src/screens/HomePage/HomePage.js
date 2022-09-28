28cd9b01a0a66c8f8da54b5cba78547c413a1882
Fayjul Islam <fayzulislam9@gmail.com>
2022-09-28 09:41:59 +0600

    trying to add navbar

---
+++ frontend/src/screens/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+
+export default function HomePage() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h1>Alllah Please help me</h1>
+    </div>
+  );
+}
