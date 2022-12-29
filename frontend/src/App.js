15dad3cfeff608f7ee0fcd072bc0916b3a84222e
Fayjul Islam <fayzulislam9@gmail.com>
2022-12-29 07:35:37 +0600

    NavBar done alhamdulillah

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import NavBar from './components/Nav/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- frontend/src/components/Nav/NavBar.js
+++ frontend/src/components/Nav/NavBar.js
@@ -8,21 +8,17 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      {/* <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar bg="primary" variant="dark">
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>STAM:CSE</Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto  w-100  justify-content-end">
-              <Link className="nav-link" to="/signin">
-                Sign In
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="/">
+            <h1>STAM:CSE</h1>
+          </Navbar.Brand>
+          <Nav.Link href="/login">
+            <h3>LOGIN</h3>
+          </Nav.Link>
         </Container>
-      </Navbar> */}
+      </Navbar>
+
       <Navbar bg="light" expand="lg">
         <Container align="center">
           <Navbar.Brand href="/">
