f9b2d100e904ed2e25cc44f45efbc1a368928802
Fayjul Islam <fayzulislam9@gmail.com>
2022-12-29 08:44:25 +0600

    Sing Up and login screen is added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,22 @@ import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import NavBar from './components/Nav/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LogIn from './screens/LogIn/LogIn';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from './screens/SignUp/SignUp';
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
+      <BrowserRouter>
+        <NavBar></NavBar>
+        <main>
+          <Routes>
+            <Route path="/login" element={<LogIn></LogIn>} />
+            <Route path="/signup" element={<SignUp></SignUp>} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
